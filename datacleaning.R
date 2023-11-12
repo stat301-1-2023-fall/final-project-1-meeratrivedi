29db23a169ecd64f9d78953d9829f0691a11bb68
Meera Trivedi <meeratrivedi2026@u.northwestern.edu>
2023-11-12 14:43:44 -0600

    more cleaning more joining more graphs

--- datacleaning.R
+++ datacleaning.R
@@ -2,18 +2,28 @@
 
 library(tidyverse)
 
+lines <- read_csv("data/friends.csv")
+info <- read_csv("data/friends_info.csv")
+emotions <- read_csv("data/friends_emotions.csv")
+
 #joining lines and emotions
 
-
 openingscene <- lines |> 
-  filter(season == "1", episode == "1", scene == "1") |> 
-  left_join(emotions |> filter(season == "1", episode == "1", scene == "1"), 
-            by = c("season" = "season"))
+  filter(season == "1", episode == "1", scene == "1")
 
 
+lines_emotions <- lines |> 
+  filter(speaker != "Scene Directions") |> 
+  left_join(emotions)
+
+lines_emotions |> 
+  count(emotion)
+
+#just monica
 monica <- lines |> 
   filter(speaker == "Monica Geller") 
 
+#just the 6 main characters
 maincharacters <- lines |> 
   filter(speaker %in% c("Monica Geller", 
                         "Ross Geller", 
@@ -22,7 +32,7 @@ maincharacters <- lines |>
                         "Joey Tribbiani", 
                         "Chandler Bing"))
 
-
+#everyone but the main characters
 guestcharacters <- lines |> 
   filter(!speaker %in% c("Monica Geller", 
                          "Ross Geller", 
@@ -42,7 +52,22 @@ read_csv(file = "data/friends.csv",
 topguestcharacters <- guestcharacters |> 
   group_by(speaker) |> 
   count(speaker) |> 
-  rename(lines = n) |> 
-  filter(lines > 10) |> 
-  mutate(lines = as.double(lines)) |> 
-  arrange(desc(lines))
+  filter(n > 10) |> 
+  arrange(desc(n)) |> 
+  rename(num_lines = n) |> 
+  slice_max(num_lines, n = 10)
+
+topguestcharacters
+
+#join lines and info
+linesinfo <- 
+lines |> 
+  full_join(info)
+
+#how many episodes did each write
+info |> 
+  count(written_by) |>
+  filter(n > 1) |> 
+  arrange(desc(n)) |> 
+  rename(episodes_written = n) |> 
+  slice_max(episodes_written, n = 10)
